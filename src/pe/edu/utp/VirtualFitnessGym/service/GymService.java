512fd1ea01747868d1d672b0008ae4baebfccb5b
utpmemo <1330566@utp.edu.pe>
2017-03-25 21:25:24 -0500

    GymService

--- src/pe/edu/utp/VirtualFitnessGym/service/GymService.java
+++ src/pe/edu/utp/VirtualFitnessGym/service/GymService.java
@@ -1,7 +1,79 @@
 package pe.edu.utp.VirtualFitnessGym.service;
 
+import pe.edu.utp.VirtualFitnessGym.models.User;
+import pe.edu.utp.VirtualFitnessGym.models.UsersEntity;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * Created by GUILLERMO on 11/03/2017.
  */
 public class GymService {
+
+    private Connection connection;
+    private UsersEntity usersEntity;
+    public GymService() {
+
+    }
+
+
+
+    public boolean validateUser(String userName, String password) {
+        return (userName.equalsIgnoreCase("admin") &&
+                password.equalsIgnoreCase("password"));
+    }
+    public boolean Registrar_Usuario(String surnames, String name,String sex, String phone, String rol , String skill) {
+
+        User   user = new User() ;
+        user.setSurname(surnames);
+        user.setName(name);
+        user.setSex(sex);
+        user.setPhone(phone);
+        user.setRol(rol);
+        user.setSkills(skill);
+
+        UsersEntity  usersEntity = new UsersEntity();
+        usersEntity.create(user);
+return  true;
+    }
+
+
+    public Connection getConnection() {
+        if(connection == null) {
+            try {
+                InitialContext ctx = new InitialContext();
+                connection = ((DataSource) ctx.lookup("jdbc/MySQLDatasourcE")).getConnection();
+            } catch (NamingException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    protected UsersEntity getUsersEntity() {
+        if(usersEntity == null) {
+            usersEntity= new UsersEntity();
+            usersEntity.setConnection(getConnection());
+        }
+        return usersEntity;
+    }
+
+    public void setUsersEntity(UsersEntity usersEntity) {
+        this.usersEntity = usersEntity;
+    }
+
+    public List<User> findAllUser() {
+        return getUsersEntity().findAll();
+    }
 }
