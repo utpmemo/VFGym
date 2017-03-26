3fbe4dd78276a2191c093ff2bc584febe3964958
utpmemo <1330566@utp.edu.pe>
2017-03-25 21:23:25 -0500

    UserEntity

--- src/pe/edu/utp/VirtualFitnessGym/models/UsersEntity.java
+++ src/pe/edu/utp/VirtualFitnessGym/models/UsersEntity.java
@@ -1,6 +1,9 @@
 package pe.edu.utp.VirtualFitnessGym.models;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by GUILLERMO on 04/03/2017.
@@ -12,10 +15,10 @@ public class UsersEntity extends  BaseEntity {
 
         if(getConnection() != null) {
             String sql = " insert into users (";
-            sql+=" surnames , names , sex , phone , rol ";
+            sql+=" surnames , names , sex , phone , rol ,skill_id";
             sql+=" ) values (";
             sql+=" '" + user.getSurname() + "' , '"+user.getName()+"' , '"+ user.getSex()+"' ," +
-                    " '"+ user.getPhone()+"' , '" + user.getRol() +"'";
+                    " '"+ user.getPhone()+"' , '" + user.getRol() +"' , '" + user.getSkills()+"'";
             sql+="";
             sql+=" ) ";
 
@@ -28,6 +31,40 @@ public class UsersEntity extends  BaseEntity {
         }
 
         return results;
+    }
+
+    public void Update(User user){
+        if (getConnection()!=null){
+            String sql = " update users set";
+            sql+=" surnames='"+ user.getSurname()+"'";
+            sql+=" names='"+ user.getName()+"'";
+            sql+=" sex='"+ user.getSex()+"'";
+            sql+=" phone='"+ user.getPhone()+"'";
+            sql+=" rol='"+ user.getRol()+"' where user_id = '"+ user.getId() + "'";
+        }
+
+    }
+
+    public List<User> findAll() {
+        String sql = "select * from users";
+        List<User> users = new ArrayList<>();
+        try {
+            ResultSet resultSet = getConnection().createStatement().executeQuery(sql);
+            while(resultSet.next()) {
+                users.add(new User(resultSet.getInt("user_id"),
+                        resultSet.getString("surnames"),
+                        resultSet.getString("names"),
+                        resultSet.getString("sex"),
+                        resultSet.getInt("phone"),
+                        resultSet.getString("rol"),
+                        resultSet.getInt("skill_id")));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
 
